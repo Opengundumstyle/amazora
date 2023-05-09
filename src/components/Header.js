3aa28b09ec69e2ba8b0ae4aebdc458def078c385
Zhile Lin <zhlin04@gmail.com>
2023-05-09 11:57:27 -0700

    header styling complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,20 +25,21 @@ const Header = () => {
            </div>
 
           {/**Right */}
-          <div className='text-white flex items-center text-xs space-x-6 mx-6'>
-             <div>
+          <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap '>
+             <div className='link'>
                 <p>hello bitch</p>
-                <p>Acount & Lists</p>
+                <p className='font-extrabold md:text-sm'>Acount & Lists</p>
              </div>
 
-             <div>
+             <div className='link'> 
                   <p>Returns</p>
-                  <p>& Orders</p>
+                  <p className='font-extrabold md:text-sm'>& Orders</p>
              </div>
 
-             <div>
+             <div className='relative link flex items-center' >
+                  <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
                   <ShoppingCartIcon className='h-10'/>
-                  <p>Basket</p>
+                  <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
              </div>
           </div>
        </div>
