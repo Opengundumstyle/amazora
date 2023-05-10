8e1882de63b4a9b2c18dca67979d563cfa9d6ce8
Zhile Lin <zhlin04@gmail.com>
2023-05-10 16:40:23 -0700

    cmod

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,19 @@ import {
 } from "@heroicons/react/outline"
 
 import {signIn,signOut,useSession} from 'next-auth/react'
-
+import { useRouter } from 'next/router'
 
 const Header = () => {
    
    const { data: session } = useSession()
-  
+   const router = useRouter()
 
   return (
     <header>
        <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
           <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
              <Image
+               onClick={()=>router.push('/')}
                src="https://links.papareact.com/f90"
                width={150}
                height={40}
