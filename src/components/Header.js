81bb45e2e8ff7b47aea9b0eb3335cb4a74b60295
Zhile Lin <zhlin04@gmail.com>
2023-05-31 19:23:40 -0700

    header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,11 +18,11 @@ const Header = () => {
 
   return (
     <header>
-       <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+       <div className='flex items-center bg-amazon_blue p-1 flex-grow py-5'>
           <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
              <Image
                onClick={()=>router.push('/')}
-               src="https://links.papareact.com/f90"
+               src="https://res.cloudinary.com/dzklgl8gn/image/upload/v1685584919/amazora-low-resolution-logo-color-on-transparent-background_g1swpl.png"
                width={150}
                height={40}
                // layout="responsive"
